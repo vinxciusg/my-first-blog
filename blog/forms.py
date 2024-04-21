3cf179abd8c4fa0d2c4d6d9902b5b30d8c204e39
vinxciusg <vinicius20240012489@alu.uern.br>
2024-04-21 18:31:25 -0300

    Added views to create/edit blog post inside the site.

---
+++ blog/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from .models import Post
+from django.shortcuts import redirect
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model = Post
+        fields = ('title', 'text',)
+    
+def post_new(request):
+     if request.method == "POST":
+         form = PostForm(request.POST)
+         if form.is_valid():
+             post = form.save(commit=False)
+             post.author = request.user
+             post.published_date = timezone.now()
+             post.save()
+             return redirect('post_detail', pk=post.pk)
+     else:
+         form = PostForm()
+     return render(request, 'blog/post_edit.html', {'form': form})
